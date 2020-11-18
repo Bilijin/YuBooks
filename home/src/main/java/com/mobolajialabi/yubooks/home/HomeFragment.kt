0204c9161f1ff781ca2b16992efa6913868b4f40
Dablaze-ufc <chukwuwauchenna@gmail.com>
2020-11-18 14:09:56 +0100

    initial project setup

---
+++ home/src/main/java/com/mobolajialabi/yubooks/home/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.mobolajialabi.yubooks.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+
+class HomeFragment : Fragment() {
+
+    private val viewModel by viewModels<HomeViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+}
