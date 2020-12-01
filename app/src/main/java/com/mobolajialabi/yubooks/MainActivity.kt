879bab74445ef1d5e7c75cfadc80e718c9ad7b98
Alabi Bolaji <alabibolaji78@gmail.com>
2020-12-01 20:25:49 +0100

    fixed signin layout #1

--- app/src/main/java/com/mobolajialabi/yubooks/MainActivity.kt
+++ app/src/main/java/com/mobolajialabi/yubooks/MainActivity.kt
@@ -2,10 +2,18 @@ package com.mobolajialabi.yubooks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import com.mobolajialabi.yubooks.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val binding : ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        Navigation.findNavController(this, R.id.fragment)
     }
 }
